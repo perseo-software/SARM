05e3fd3bfa21252ddcf22b1bf57f51c993ea721f
VictorCadix <31282394+VictorCadix@users.noreply.github.com>
2020-04-02 13:18:48 +0200

    Pizzometro añadido

--- SARM.ino
+++ SARM.ino
@@ -2,12 +2,17 @@
 
 #define pinPotenciometro A0
 #define pinEnableMotor 5
+#define pinPizzometro1 9
+#define pinPizzometro2 10
 
 Adafruit_BMP280 bmp; // I2C
 
 float presion; // Almacena la presion atmosferica (Pa)
 float temperatura; // Almacena la temperatura (oC)
 int altitud; // Almacena la altitud (m) (se puede usar variable float)
+
+bool pizzometro1_state;
+bool pizzometro2_state;
 
 void setup(){
     Serial.begin(9600);
@@ -17,6 +22,9 @@ void setup(){
     }
     pinMode(pinEnableMotor, OUTPUT);
     analogWrite(pinEnableMotor, 0);
+
+    pinMode(pinPizzometro1, INPUT);
+    pinMode(pinPizzometro2,INPUT);
 }
 
 void loop(){
@@ -25,16 +33,24 @@ void loop(){
     temperatura = bmp.readTemperature();
     altitud = bmp.readAltitude (1015); // Ajustar con el valor local
 
-    Serial.print(F("Presion: "));
-    Serial.print(presion);
-    Serial.print(" hPa");
-    Serial.print("\t");
-    Serial.print(("Temp: "));
-    Serial.print(temperatura);
-    Serial.println(" *C");
-    delay(100);
+    pizzometro1_state = digitalRead(pinPizzometro1);
+    pizzometro2_state = digitalRead(pinPizzometro2);
+
+    Serial.print(pizzometro1_state);
+    Serial.print(',');
+    Serial.println(pizzometro2_state);
+
+    //Serial.print(F("Presion: "));
+    //Serial.print(presion);
+    //Serial.print(" hPa");
+    //Serial.print("\t");
+    //Serial.print(("Temp: "));
+    //Serial.print(temperatura);
+    //Serial.println(" *C");
+    //delay(100);
     
     int val = analogRead(pinPotenciometro);
     analogWrite(pinEnableMotor, val/4);
-    //Serial.println(val);    
+    //Serial.println(val);
+
 }   
